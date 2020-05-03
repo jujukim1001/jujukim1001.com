6e3376e30bef0cafea380cd116aab8db131b67fd
Steve Chang <steadyaction@gmail.com>
2020-05-02 22:59:42 -0700

    animations

--- js/index.js
+++ js/index.js
@@ -1,12 +1,40 @@
 const text = "Designing for ";
 const accentText = "Impact";
+var everMouseentered = false;
+var mouseEnteredRecently = false;
 
 $("#introductionTitle").ready(function (e) { 
-    runTyping(0);
+    runTyping(1);
+});
+
+$("#arrowImage").mouseenter(function (e) {
+    if (mouseEnteredRecently) {
+        return;
+    }
+    everMouseentered = true;
+    mouseEnteredRecently = true;
+    
+    $("#arrowImage").animate({
+        'marginTop': "30px"
+    });
+
+    setTimeout(function() {
+        mouseEnteredRecently = false;
+    }, 1000);
+});
+
+$("#arrowImage").mouseleave(function (e) {
+    $("#arrowImage").animate({
+        'marginTop': "0px"
+    });
 });
 
 function runTyping(index) {
     if (index > text.length + accentText.length) {
+        if (!everMouseentered){
+            console.log(everMouseentered);
+            $("#arrowImage").toggleClass("bounce");
+        }
         return;
     }
     var result = "";
